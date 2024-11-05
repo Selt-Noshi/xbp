eb56b9e1178d2e99b8e6feeb30d9f1730ed18650
Selt-Noshi <r202402292us@jindai.jp>
2024-11-05 15:50:10 +0900

    20241105-08

---
+++ de12/python/main.py
@@ -0,0 +1,17 @@
+age=int(input("age"))
+name=input("name")
+waist=float(input("waist"))
+hight=int(input("high"))
+
+print(name,"さんは腹囲", waist, "cmで身長は",hight,"cmで年齢は",age,"オですね。")
+
+if waist>=85:
+    print("Mr.",name,"you are a fat ass")
+else:
+    print("Mr.",name,"plz go eat some shit")
+
+if hight>=180:
+    print("Mr.",name,"i'm going to cut your leg off")
+
+if age<=0:
+    print("Mr.",name,"what the hell are you")
